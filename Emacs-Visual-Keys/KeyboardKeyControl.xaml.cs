7e54f9a2d46fa84616bade54fde83533bf2a3711
Bobbias <bobbias@gmail.com>
2020-10-17 21:19:02 -0400

    Add project files.

---
+++ Emacs-Visual-Keys/KeyboardKeyControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace EmacsVisualKeys
+{
+    /// <summary>
+    /// Interaction logic for KeyboardKeyControl.xaml
+    /// </summary>
+    [ContentProperty("AdditionalContent")]
+    public partial class KeyboardKeyControl : UserControl
+    {
+        public KeyboardKeyControl()
+        {
+            InitializeComponent();
+        }
+
+        static KeyboardKeyControl()
+        {
+            AdditionalContentProperty =
+DependencyProperty.Register("AdditionalContent", typeof(object), typeof(KeyboardKeyControl),
+new PropertyMetadata(null));
+        }
+
+        /// <summary>
+        /// Gets or sets additional content for the UserControl
+        /// </summary>
+        public object AdditionalContent
+        {
+            get { return GetValue(AdditionalContentProperty); }
+            set { SetValue(AdditionalContentProperty, value); }
+        }
+        public static readonly DependencyProperty AdditionalContentProperty;
+    }
+}
